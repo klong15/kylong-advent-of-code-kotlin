091db3b1812f893db161027a77b67ac9371d0bc5
Kyle Long <klong0815@gmail.com>
2024-12-03 23:05:03 -0600

    Finished part 1 with correct answer.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,27 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+        var a = input[0]
+
+        input.forEach {
+            var result = it.split(regex = "\\s+".toRegex())
+            leftList.add(result[0].toInt())
+            rightList.add(result[1].toInt())
+        }
+
+        leftList.sort()
+        rightList.sort();
+
+        var distance = 0
+        for(i in leftList.indices) {
+            distance += abs(leftList[i] - rightList[i])
+        }
+
+
+        return distance
     }
 
     fun part2(input: List<String>): Int {
@@ -8,14 +29,14 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
